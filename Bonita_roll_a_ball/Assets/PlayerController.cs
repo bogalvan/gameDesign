ae20b61af9e8d6d37bc81b481dd05eac65d527ae
Bonita <bogalvan6@gmail.com>
2019-01-09 19:24:21 -0800

    Roll a Ball Added

---
+++ Bonita_roll_a_ball/Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb; //refrence to the rigidbody component of the Player 
+    private int count; //counts number of items collected
+
+    //UI
+    public Text countText;
+    public Text winText;
+
+    void Start(){ //all code in start method gets called in first frame of the game 
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    //update is called before rendering a frame
+    //fixed update is where physics code should go 
+
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+}
