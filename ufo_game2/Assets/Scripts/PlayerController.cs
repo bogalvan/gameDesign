a42a4cfc1d7f0f96ed7f6d703378b1d57a8c17b0
bonita galvan <bogalvan6@gmail.com>
2019-01-24 12:06:16 -0800

    ufo2

---
+++ ufo_game2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 10;
+    private int count;
+    private int lives;
+    private Rigidbody2D rb2d;
+    private float waitTime = 5.0f;
+    SpriteRenderer m_SpriteRenderer;
+    Color speedColor = new Color(0, 255, 250);
+    private bool gameOver = false;
+ 
+
+    public Text countText;          
+    public Text winText;
+    public Text livesText;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        speed = 10;
+        count = 0;
+        lives = 3;
+
+        winText.text = "";
+        SetCountText();
+        SetLivesText();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
+
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp") && gameOver == false)
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Speed") && gameOver == false)
+        {
+            other.gameObject.SetActive(false);
+
+            speed = 50;
+            m_SpriteRenderer.color = speedColor;
+            yield return new WaitForSeconds(2.0f);
+            speed = 10;
+            m_SpriteRenderer.color = Color.white;
+        }
+        if (other.gameObject.CompareTag("Cow"))
+        {
+            speed = 0;
+            m_SpriteRenderer.color = Color.red;
+            lives--;
+            SetLivesText();
+            yield return new WaitForSeconds(1.50f);
+            m_SpriteRenderer.color = Color.white;
+            if(gameOver == false)
+            {
+                speed = 10;
+            }
+        }
+    }
+
+    void SetLivesText()
+    {
+
+        livesText.text = "Lives: " + lives.ToString();
+
+        if (lives <= 0)
+        {
+            winText.text = "You Loose!";
+            speed = 0;
+            gameOver = true;
+        }
+
+    }
+
+
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+            winText.text = "You win!";
+    }
+
+}
---
+++ ufo_game2/Assets/badPickup.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class badPickup : MonoBehaviour
+{
+
+    private Rigidbody2D rb; //refrence to the rigidbody component of the Player 
+
+    private float moveX;
+    private float moveY;
+    private float deltaX;
+    private float deltaY;
+
+
+
+    void Start()
+    { //all code in start method gets called in first frame of the game 
+        rb = GetComponent<Rigidbody2D>();
+        moveX = Random.Range(-10f, 10f);
+        moveY = Random.Range(-10f, 10f);
+        deltaX = Random.Range(-.1f, .1f);
+        deltaY = Random.Range(-.1f, .1f);
+    }
+
+    //update is called before rendering a frame
+    //fixed update is where physics code should go 
+
+    void FixedUpdate()
+    {
+
+        moveX += deltaX;
+        moveY += deltaY;
+        Vector3 move = new Vector3(moveX, moveY, 0.0f);
+        rb.MovePosition(move);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("LRWalls"))
+        {
+            deltaX *= -1;
+        }
+        if (other.gameObject.CompareTag("TBWalls"))
+        {
+            deltaY *= -1;
+        }
+    }
+
+
+}
