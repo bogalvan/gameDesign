c9150e0c8a250bcb44b77a100414c127829cbe60
bonita galvan <bogalvan6@gmail.com>
2019-02-05 14:59:13 -0800

    pong updates

---
+++ Pong/Assets/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    void OnMouseDown()
+    {
+        print("pressed");
+        GameManager.buttonClicked();
+
+    }
+
+
+}
--- Pong/Assets/Scripts/GameManager.cs
+++ Pong/Assets/Scripts/GameManager.cs
@@ -1,21 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
+    public enum GameState { startMenu, gamePlay, restartMenu }
+    public static GameState gameState = GameState.startMenu;
     public static int PlayerScore1 = 0;
     public static int PlayerScore2 = 0;
 
-    public GUISkin layout;
+    //UI
+    public Text startText;
+    public static Text p1Text;
+    public static Text p2Text;
 
-    GameObject theBall;
+    private static GameObject button;
+    private static GameObject game;
+
+    private void Awake()
+    {
+        //DontDestroyOnLoad(this.gameObject);
+    }
 
     // Use this for initialization
     void Start()
     {
-        theBall = GameObject.FindGameObjectWithTag("Ball");
+        button = GameObject.FindGameObjectWithTag("Button");
+        game = GameObject.FindGameObjectWithTag("gameObjects");
+
+        p1Text.text = "";
+        p2Text.text = "";
+
+    }
+
+    public void Update()
+    {
+        if(gameState == GameState.startMenu)
+        {
+            game.SetActive(false);
+        }
+        //print(gameState);
+        if(gameState == GameState.gamePlay)
+        {
+            button.SetActive(false);
+            startText.text = "";
+            game.SetActive(true);
+        }
+    }
+
+    public static void buttonClicked()
+    {
+        print("changing game state");
+        gameState = GameState.gamePlay;
+
     }
 
     public static void Score(string wallID)
@@ -24,13 +63,17 @@ public class GameManager : MonoBehaviour
         {
             print("1");
             PlayerScore1++;
+            p1Text.text = PlayerScore1.ToString();
         }
         else
         {
             print("2");
             PlayerScore2++;
+            p2Text.text = PlayerScore2.ToString();
         }
     }
 
 
 }
+
+
